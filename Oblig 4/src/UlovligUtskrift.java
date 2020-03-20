b607bcbbd5fe64a908aad4232d0889d6808be2a0
hermannlindgren <hermannlindgren@hermanns-mbp.lan>
2020-03-20 17:34:01 +0100

    Lagde klassen UlovligUtskrift som man trenger til D

---
+++ Oblig 4/src/UlovligUtskrift.java
@@ -0,0 +1,7 @@
+
+public class UlovligUtskrift extends Exception{
+	UlovligUtskrift(Lege l, Legemiddel lm){
+		super("Legen " + l.hentNavnLege() + " har ikke lov til å skrive ut " + lm.hentNavn());
+	}
+
+}
