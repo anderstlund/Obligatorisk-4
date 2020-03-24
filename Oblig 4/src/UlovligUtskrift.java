1f308ce4f5e5bf3383737fc99aba9ba7ac50e112
hermannlindgren <hermannlindgren@hermanns-mbp.lan>
2020-03-24 15:57:39 +0100

    jobber med d3, sjekk discord

--- Oblig 4/src/UlovligUtskrift.java
+++ Oblig 4/src/UlovligUtskrift.java
@@ -1,5 +1,10 @@
 
 public class UlovligUtskrift extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	UlovligUtskrift(Lege l, Legemiddel lm){
 		super("Legen " + l.hentNavnLege() + " har ikke lov til å skrive ut " + lm.hentNavn());
 	}
